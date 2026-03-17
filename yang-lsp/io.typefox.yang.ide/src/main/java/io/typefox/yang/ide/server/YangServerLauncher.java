1892b372a376d8936c011078d2401b50674e2a6a
Siddharth Sharma <siddharth.sharma@ericsson.com>
2026-03-17 01:35:55 +0100

    Bind YangProjectManager in plain server so excludePath setting is respected

--- yang-lsp/io.typefox.yang.ide/src/main/java/io/typefox/yang/ide/server/YangServerLauncher.java
+++ yang-lsp/io.typefox.yang.ide/src/main/java/io/typefox/yang/ide/server/YangServerLauncher.java
@@ -1,6 +1,7 @@
 package io.typefox.yang.ide.server;
 
 import org.eclipse.xtext.ide.server.LanguageServerImpl;
+import org.eclipse.xtext.ide.server.ProjectManager;
 import org.eclipse.xtext.ide.server.ServerLauncher;
 import org.eclipse.xtext.ide.server.ServerModule;
 
@@ -9,6 +10,7 @@ public class YangServerLauncher {
 	public static void main(String[] args) {
 		ServerLauncher.launch(YangServerLauncher.class.getName(), args, new ServerModule(), binder -> {
 			binder.bind(LanguageServerImpl.class).to(YangLanguageServerImpl.class);
+			binder.bind(ProjectManager.class).to(YangProjectManager.class);
 		});
 	}
 }
