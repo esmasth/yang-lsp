058590dd0a347552e53f49399c8cf57131dd4bbf
Siddharth Sharma <siddharth.sharma@ericsson.com>
2026-03-31 11:52:45 +0200

    Add Apache-2.0 license headers to new files

--- yang-lsp/io.typefox.yang.ide/src/main/java/io/typefox/yang/ide/server/YangServerLauncher.java
+++ yang-lsp/io.typefox.yang.ide/src/main/java/io/typefox/yang/ide/server/YangServerLauncher.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2017-2020 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.typefox.yang.ide.server;
 
 import org.eclipse.xtext.ide.server.LanguageServerImpl;
